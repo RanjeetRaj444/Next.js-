2444cfa56cd277d2c52af249aafc3fa42699426c
ranjeetraj444 <ranjeetraj44666@gmail.com>
2024-05-23 12:23:02 +0530

    Pathch and DELETE request completed.

---
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -0,0 +1,52 @@
+import { comments } from "../data";
+//dynamic routes for finding particular product.
+export async function GET(
+	_request: Request,
+	{
+		params,
+	}: {
+		params: {
+			id: string;
+		};
+	},
+) {
+	const comment = comments.find(
+		(comment) => comment.id === parseInt(params.id),
+	);
+	return Response.json(comment);
+}
+export async function PATCH(
+	request: Request,
+	{
+		params,
+	}: {
+		params: {
+			id: string;
+		};
+	},
+) {
+	const body = await request.json();
+	const { text } = body;
+	const comment = comments.findIndex(
+		(comment) => comment.id === parseInt(params.id),
+	);
+	comments[comment].text = text;
+	return Response.json(comments[comment]);
+}
+export async function DELETE(
+	_request: Request,
+	{
+		params,
+	}: {
+		params: {
+			id: string;
+		};
+	},
+) {
+	const comment = comments.findIndex(
+		(comment) => comment.id === parseInt(params.id),
+	);
+	const deletedComment = comments[comment];
+	comments.splice(comment, 1);
+	return Response.json(deletedComment);
+}
